ca682590a9e8391aa1379a1cd8a90c859b73dfda
Catherine <catherineep4189@gmail.com>
2020-01-20 16:36:14 +0000

    change todoTask to an empty array and when new task is generated into the array, it displays to css formatting or do not display any content at all, if array is empty

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,12 +1,34 @@
 import React from 'react'
 
-const TodoList = () => {
+const TodoList = ({ todoTask }) => {
+    const list = todoTask.length ?(
+        todoTask.map(task => {
+        return <div className="container">
+            <div className="row">
+                <div className="col-12 cp-3 text-center tasklist">
+                    <div className="card" key={task.id}>
+                        {task}
+                        <div>
+                            <button class="taskbuttons"><i class="far fa-check-circle"></i></button>
+                            <button class="taskbuttons"><i class="far fa-trash-alt"></i></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    }) 
+    ) : (
+        null
+    )
+
     return (
         <div className="list">
-
+            {list}
         </div>
     )
 }
+
+
 
 
 export default TodoList
