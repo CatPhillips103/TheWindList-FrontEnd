c34f4cc14b047e1da1671be412ede12e425078e1
Catherine <catherineep4189@gmail.com>
2020-01-20 14:57:01 +0000

    TodoList component added

--- src/App.js
+++ src/App.js
@@ -2,23 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './Header';
 import AddTodo from './addTodo'
+import TodoList from './TodoList'
 
 class App extends Component {
   state = {
-    todoTask: [
-      
-    ]
+    todoTask: []
   }
-
-
-
 
 
   render() {
     return (
       <div>
         <Header />
-        <AddTodo />
+        <AddTodo  />
+        <TodoList />
+
       </div>
     )
   }
