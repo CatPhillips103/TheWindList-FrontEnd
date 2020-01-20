26095e53ddb8613783de9cab0fab464c9b420eaa
Catherine <catherineep4189@gmail.com>
2020-01-20 19:46:13 +0000

    comments added to app

--- src/App.js
+++ src/App.js
@@ -5,24 +5,33 @@ import AddTodo from './addTodo'
 import TodoList from './TodoList'
 
 class App extends Component {
+  // empty array props: todoTask state will change when a new task is added using either enter or
+  // plus button will update the state to have content inside
+
   state = {
     todoTask: []
   }
 
+
+  // addTodoTask takes in a task as a function value which a new variable is assign using
+  // an array which has accesses the current state of the todoTask, whilst adding new task onto the array
+
+
   addTodoTask = (task) => {
+    // each task has unique id for keys within React
     task.id = Math.random();
     let tasks = [...this.state.todoTask, task];
     this.setState({
-      todoTask: tasks 
-    })
+      todoTask: tasks
+    });
   }
 
   render() {
     return (
       <div>
         <Header />
-        <AddTodo addTodoTask={this.addTodoTask}  />
-        <TodoList todoTask={this.state.todoTask}/>
+        <AddTodo addTodoTask={this.addTodoTask} />
+        <TodoList todoTask={this.state.todoTask} />
 
       </div>
     )
--- src/addTodo.js
+++ src/addTodo.js
@@ -1,30 +1,34 @@
 import React, { Component } from 'react'
 
 class AddTodo extends Component {
+    // the state of the taskform content is currently empty, however using the onChange event
+    // value entered in the form will change the state of the empty string content.
+
     state = {
-      content: ' '
+        content: ' '
     }
 
     appChange = (e) => {
-    this.setState({
-        content: e.target.value
-    })
-    } 
+        this.setState({
+            content: e.target.value
+        });
+    }
 
+    // accessing the props which is a card that displays tasks and completed / deleted buttons
 
     taskSubmit = (e) => {
-      e.preventDefault(); 
-      this.props.addTodoTask(this.state);
+        e.preventDefault();
+        this.props.addTodoTask(this.state);
     }
 
     render() {
         return (
-          <div className="container my-4 taskform-container">
-              <form onSubmit={this.taskSubmit} className="col-sm-12 taskform">
-                  <input type="text" className="col-sm-8 text-center tasks" placeholder="travel with ease..." onChange={this.appChange}/>
-                  <button className="addButton"><i className="fas fa-plus-circle"></i></button>
-              </form>
-          </div>
+            <div className="container my-4 taskform-container">
+                <form onSubmit={this.taskSubmit} className="col-sm-12 taskform">
+                    <input type="text" className="col-sm-8 text-center tasks" placeholder="travel with ease..." onChange={this.appChange} />
+                    <button className="addButton"><i className="fas fa-plus-circle"></i></button>
+                </form>
+            </div>
         )
     }
 }
