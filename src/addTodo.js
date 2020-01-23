231ebb72f67825f463f4ac01b1b3809213e1ad78
Catherine <catherineep4189@gmail.com>
2020-01-23 13:03:45 +0000

    added submit event to allow any value to also update state of taskContentvia enter key and to prevent page refreshing bug

--- src/addTodo.js
+++ src/addTodo.js
@@ -4,9 +4,13 @@ class AddTodo extends Component {
 
 
     state = {
-        taskContent: 'add task'
+        taskContent: ''
     }
 
+    enterSubmit = (e) => {
+        e.preventDefault();
+        this.props.addTodo(this.state.taskContent);
+    }
 
     clickButton = (e) => {
         e.preventDefault();
@@ -24,7 +28,7 @@ class AddTodo extends Component {
     render() {
         return (
             <div className="container my-4 taskform-container">
-                <form className="col-sm-12 taskform">
+                <form className="col-sm-12 taskform" onSubmit={this.enterSubmit}>
                     <input type="text" className="col-sm-8 text-center tasks" placeholder="travel with ease..." onChange={this.newContent} />
                     <button onClick={this.clickButton} type="button" className="addButton"><i className="fas fa-plus-circle"></i></button>
                 </form>
